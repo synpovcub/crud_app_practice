a56cd199ae7a7347488dbecdc11a972a839aebc7
synpovcub <synpovcub@gmail.com>
2026-03-04 15:16:53 -0600

    Create db and update gitignore

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
+from datetime import datetime
 
 app = Flask(__name__)
+Scss(app)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+
+#Data Class ~ Row Data
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable = False)
+    complete = db.Column(db.Integer, default = 0)
+    created = db.Column(db.DateTime(timezone = True), default = func.now())
+
+
+    def __repr__(self) ->str:
+        return f"Task{self.id}"
+
+
+#Routes to webpages
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -11,4 +31,7 @@ def index():
 
 
 if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+
     app.run(debug = True)
